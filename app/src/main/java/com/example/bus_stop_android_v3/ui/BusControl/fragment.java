647e3a5880ad4fc4e79cb848c6c920e354f6072c
Yashua D. Evans <yashua@Yashuas-iMac.local>
2020-06-04 14:41:54 -0400

    Basic Framework of v3.0

---
+++ app/src/main/java/com/example/bus_stop_android_v3/ui/BusControl/fragment.java
@@ -0,0 +1,34 @@
+package com.example.bus_stop_android_v3.ui.BusControl;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.bus_stop_android_v3.R;
+
+public class fragment extends androidx.fragment.app.Fragment {
+
+    private model homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(model.class);
+        View root = inflater.inflate(R.layout.driver_bus_fragment, container, false);
+        final TextView textView = root.findViewById(R.id.text_busController);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/bus_stop_android_v3/ui/Settings/model.java
@@ -0,0 +1,19 @@
+package com.example.bus_stop_android_v3.ui.Settings;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class model extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public model() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is settings fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
