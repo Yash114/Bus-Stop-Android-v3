66c323047204c7eb1c83c5be491c1d32815f0e11
yashua <yashevans57@gmail.com>
2020-06-05 09:04:49 -0400

    added UI to Drivermode

--- app/src/main/java/com/gingergear/BusStopv3/ui/BusControl/fragment.java
+++ app/src/main/java/com/gingergear/BusStopv3/ui/BusControl/fragment.java
@@ -22,13 +22,6 @@ public class fragment extends androidx.fragment.app.Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(model.class);
         View root = inflater.inflate(R.layout.driver_bus_fragment, container, false);
-        final TextView textView = root.findViewById(R.id.text_busController);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
